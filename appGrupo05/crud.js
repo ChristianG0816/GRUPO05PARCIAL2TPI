9a0eeab39d25f7556781c8b7061296acfe668700
ChristianG0816 <86392615+ChristianG0816@users.noreply.github.com>
2021-11-23 13:20:15 -0600

    Add files via upload

---
+++ appGrupo05/crud.js
@@ -0,0 +1,9 @@
+/*  Grupo 05 Integrantes:
+    Miguel Angel Amaya Rodriguez
+    Christian Javier Ayala Guerra
+    Diego José Ayala Guerra
+    Luis Alonso Cornejo Jiménez
+    Carolina Isabel Pineda Delgado
+    José Gustavo Pineda Delgado
+    William Enrique Vásquez Mancia
+*/
